44c465415a196d327b4983c3a92509e86553964f
Suryansh Chaudhary <suryanshchaudhary46@gmail.com>
2026-09-26 02:49:02 +0530

    Fix root detection class name

--- driver_app/lib/core/security/root_detection.dart
+++ driver_app/lib/core/security/root_detection.dart
@@ -6,8 +6,8 @@ class RootDetection {
   /// Check if device is rooted (Android) or jailbroken (iOS)
   static Future<bool> isDeviceRooted() async {
     try {
-      final bool jailbroken = await FlutterJailbreakDetection.jailbroken;
-      final bool developerMode = await FlutterJailbreakDetection.developerMode;
+      final bool jailbroken = await FlutterJailbreakDetectionPlus.jailbroken;
+      final bool developerMode = await FlutterJailbreakDetectionPlus.developerMode;
       
       if (jailbroken || developerMode) {
         AppLogger.warning('SECURITY RISK: Root/Jailbreak or Developer Mode detected!');
